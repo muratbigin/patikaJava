9d6da43612d3a674313200f1cd72b31e738e97ee
muratbigin <muratbigin00@gmail.com>
2021-11-26 22:10:04 +0100

    refactoring

---
+++ src/java101/BlaBla.java
@@ -0,0 +1,38 @@
+package java101;
+
+public class BlaBla {
+    public static void main(String[] args) {
+        ////        float flo =3.14f;     // f  double olmadigini belirler
+////        System.out.println(flo);
+////        double dou = 3.14;
+////        System.out.println(dou);
+////        double dou2 =1;
+//
+/////////////////////////////////////////////////////////////////////////////////
+//
+//        char cha = 'b';
+//        //System.out.println(cha);
+//
+//        char cha2 = 98;
+//        // System.out.println(cha2);
+//
+//        char c1 = 'J';
+//        char c2 = 'A';
+//        char c3 = 'V';
+//        char c4 = 'A';
+////        System.out.println(c1+c2+c3+c4);  // aski karsiliklarini baz aldi
+////        System.out.println("" + c1 +c2+c3+c4); //"" string olarak yazmasini sagladi
+//
+//        int a = 1, b = 2 , y=3;
+//        //b=b+a;
+//        //b+=a;
+//        //b-=a;
+//        //b*=a;
+//
+//       // System.out.println(y=-12%-5);
+//
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("bir say lutfen");12
+//        a= in.nextInt();
+    }
+}
